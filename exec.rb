b20be2eb772e19006b8a757616ed443205694a65
Shotaro Sasaki <fspia.biz@gmail.com>
2016-04-18 07:11:04 +0900

    exec

---
+++ exec.rb
@@ -0,0 +1,60 @@
+
+class Fizz
+
+	def show
+		p "Fizz!"
+	end
+
+end
+
+class Buzz
+
+	def show
+		p "Buzz!"
+	end
+
+end
+
+class GitHub
+
+	def show
+		p "GitHub!"
+	end
+
+end
+
+class FizzBuzz
+
+	def show
+		p "FizzBuzz!"
+	end
+
+end
+
+class FizzBuzzExecutor
+
+	def initialize(no)
+		@no = no
+	end
+
+	def show
+		if @no % 3 == 0 && @no % 5 == 0
+			FizzBuzz.new.show
+		elsif @no % 3 == 0
+			Fizz.new.show
+		elsif @no % 5 == 0
+			Buzz.new.show
+		elsif @no % 7 == 0
+			GitHub.new.show
+		else
+			p @no
+		end
+	end
+
+end
+
+no = ARGV[0].to_i
+
+fb = FizzBuzzExecutor.new(no)
+fb.show()
+
